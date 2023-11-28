25536d494aab2d7b5249c1401f439f821d0a1897
Bay <bayurkp194@gmail.com>
2023-11-28 12:13:20 +0800

    Add typer

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
 import os
 from PIL import Image, ImageEnhance
+import typer
+from typing_extensions import Annotated
 
 
-#  ImageEnhance.Color(image)
-#  ImageEnhance.Contrast(image)
-#  ImageEnhance.Brigthness(image)
-#  ImageEnhance.Sharpness(image)
+def main(process: Annotated[str, typer.Option(help="Processes for aglonema images (saturation/contrast/brigthness/shrapness)",
+                                              prompt=True)] = "saturation",
+         factor: Annotated[float, typer.Option(help="Processes for aglonema images (saturation/contrast/brigthness/shrapness)",
+                                               prompt=True)] = 1.0):
 
+    if process not in ["saturation", "contrast", "brigthness", "sharpness"]:
+        print("No process matches.")
+        exit()
 
-def main():
     rootPath = "aglonema/"
     pathList = [rootPath + path + "/" for path in os.listdir(rootPath)]
+    totalFiles = sum([len(files) for _, _, files in os.walk(rootPath)])
 
-    for path in pathList:
-        for image in os.listdir(path):
-            imageFileName = os.fsdecode(image)
-            imagePath = path + imageFileName
+    print(
+        f"Processing images with {process} adjustment by a factor of {factor}.")
+    with typer.progressbar(range(totalFiles), label=f"Processing ...") as progress:
+        for path in pathList:
+            for image in os.listdir(path):
+                imageFileName = os.fsdecode(image)
+                imagePath = path + imageFileName
 
-            # Saturation
-            image = Image.open(imagePath)
-            enhancer = ImageEnhance.Color(image)
-            factor = 5
-            output = enhancer.enhance(factor)
+                image = Image.open(imagePath)
+                if process == "saturation":
+                    process = "color"
+                enhancer = eval(f"ImageEnhance.{process.capitalize()}(image)")
+                output = enhancer.enhance(factor)
 
-            if not os.path.exists(f"output/saturation/{factor}/{path}"):
-                os.makedirs(f"output/saturation/{factor}/{path}")
-            output.save(f"output/saturation/{factor}/{imagePath}")
+                if process == "color":
+                    process = "saturation"
+                if not os.path.exists(f"output/{process}/{factor}/{path}"):
+                    os.makedirs(f"output/{process}/{factor}/{path}")
+                output.save(f"output/{process}/{factor}/{imagePath}")
+                progress.update(1)
+    print(f"Dang! Processed {totalFiles} images.")
+    print(
+        f"Saved in {os.path.abspath(os.getcwd())}/output/{process}/{factor}/")
 
 
 if __name__ == "__main__":
-    main()
+    print(r"""
+          
+ ________  ________  ___       ________  ________  ___       ___     
+|\   __  \|\   ____\|\  \     |\   __  \|\   ____\|\  \     |\  \    
+\ \  \|\  \ \  \___|\ \  \    \ \  \|\  \ \  \___|\ \  \    \ \  \   
+ \ \   __  \ \  \  __\ \  \    \ \  \\\  \ \  \    \ \  \    \ \  \  
+  \ \  \ \  \ \  \|\  \ \  \____\ \  \\\  \ \  \____\ \  \____\ \  \ 
+   \ \__\ \__\ \_______\ \_______\ \_______\ \_______\ \_______\ \__\
+    \|__|\|__|\|_______|\|_______|\|_______|\|_______|\|_______|\|__|
+    
+    """)
+    typer.run(main)
